dd101da86a4f9dd05ae8b72699a69f0ad92a4c0a
MaxSM <maxi.sm117@gmail.com>
2024-04-25 11:23:46 -0300

    Manejo de estado de carga con el archivo loading.tsx

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,21 +1,37 @@
-const getProducts = async () => {
-  const res = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
-    headers: new Headers ({
-      'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
-    })
-  });
-  const data = await res.json();
-  return data;
-}
+import Image from 'next/image'
+import styles from './MainProducts.module.sass'
 
+const getProducts = async () => {
+  try {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+      headers: new Headers({
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+      })
+    })
+    const { products } = await response.json()
+    return products
+  } catch (error) {
+    console.log(error);
+  }
+}
 // Ventaja de los server components en Next.js => Pueden ser async
 export const MainProducts = async () => {
-  const products = await getProducts();
-  console.log(products);
-  
+  const products = await getProducts()
+  // console.log(products)
   return (
-    <section>
-      <h1>Main Products</h1>
+    <section className={styles.MainProducts}>
+      <h3>✨ New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products?.map((product) => {
+          const imageSrc = product.images[0].src;
+          return (
+            <article key={product.id}>
+              <p>{product.title}</p>
+              <Image src={imageSrc} fill alt={product.title} loading="eager" />
+            </article>
+          )
+        })}
+      </div>
     </section>
-  );
-};
+  )
+}
