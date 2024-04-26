1b2e4c79930f632e695a8b55ddfb863e421f4647
MaxSM <maxi.sm117@gmail.com>
2024-04-26 18:53:37 -0300

    filtrando categorías de productos

--- src/services/shopify/collections.ts
+++ src/services/shopify/collections.ts
@@ -27,3 +27,28 @@ export const getCollections = async () => {
     console.log(error)
   }
 }
+
+/**
+ * This TypeScript function fetches products belonging to a specific collection from a Shopify store
+ * using the provided collection ID.
+ * @param {string} id - The `id` parameter in the `getCollectionProducts` function is a string that
+ * represents the identifier of a collection in a Shopify store. This function is designed to fetch the
+ * products belonging to the specified collection using the Shopify API.
+ * @returns The `getCollectionProducts` function is returning the products belonging to a specific
+ * collection identified by the `id` parameter. The products are fetched from a Shopify API endpoint
+ * using the provided `id` and Shopify access token. If the request is successful, the function returns
+ * the array of products. If an error occurs during the process, the error is logged to the console.
+ */
+export const getCollectionProducts = async (id: string) => {
+    try {
+      const response = await fetch(shopifyUrls.collections.products(id), {
+        headers: new Headers({
+          'X-Shopify-Access-Token': env.SHOPIFY_TOKEN
+        })
+      })
+      const { products } = await response.json()
+      return products
+    } catch (error) {
+      console.log(error)
+    }
+  }
