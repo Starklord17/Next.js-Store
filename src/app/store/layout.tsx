d32e72f80c7fb9a24754886bf1e02db67f5af58d
MaxSM <maxi.sm117@gmail.com>
2024-04-29 18:45:19 -0300

    flujo de login

--- src/app/store/layout.tsx
+++ src/app/store/layout.tsx
@@ -1,23 +1,40 @@
-import { getCollections } from "app/services/shopify/collections"
-import Link from "next/link"
+import { getCollections } from "app/services/shopify/collections";
+import Link from "next/link";
+import styles from "./StoreLayout.module.sass";
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
-  const collections = await getCollections()
+/**
+ * The function `Layout` is an async React component that fetches collections data and renders a layout
+ * with navigation links and children components.
+ * @param  - The `Layout` component is an asynchronous function that takes in a single parameter as an
+ * object. The parameter object has a property `children` which is of type `React.ReactNode`.
+ * @returns The Layout component is being returned, which includes a main element with a heading
+ * "Explore", a navigation section with a list of collections as links, and the children components
+ * passed to the Layout component.
+ */
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const collections = await getCollections();
 
-/* Inside the component, it is fetching collections asynchronously using the `getCollections` function. Once the collections are
-fetched, it renders a `<main>` element containing a navigation bar (`<nav>`) and the children components passed to the `Layout` component. */
   return (
-    <main>
+    <main className={styles.StoreLayout}>
+      <h1>Explore</h1>
       <nav>
-        {
-          collections.map((collection: any) => (
-            <Link key={collection.id} href={'/store/' + collection.handle}>
+        <ul className={styles.StoreLayout__list}>
+          {collections.map((collection: any) => (
+            <Link
+              key={collection.id}
+              href={"/store/" + collection.handle}
+              className={styles.StoreLayout__chip}
+            >
               {collection.title}
             </Link>
-          ))
-        }
+          ))}
+        </ul>
       </nav>
       {children}
     </main>
-  )
+  );
 }
